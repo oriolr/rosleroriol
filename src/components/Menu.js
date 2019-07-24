4e4106130579ecf50132e2b58d806ace60da737f
Rosler <oriolr32@gmail.com>
2019-07-23 20:29:03 -0400

    Project and Banner update

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -7,13 +7,9 @@ const Menu = (props) => (
         <div className="inner">
             <ul className="links">
                 <li><Link onClick={props.onToggleMenu} to="/">Home</Link></li>
-                <li><Link onClick={props.onToggleMenu} to="/landing">Landing</Link></li>
-                <li><Link onClick={props.onToggleMenu} to="/generic">Generic</Link></li>
-                <li><Link onClick={props.onToggleMenu} to="/elements">Elements</Link></li>
-            </ul>
-            <ul className="actions vertical">
-                <li><a href="#" className="button special fit">Get Started</a></li>
-                <li><a href="#" className="button fit">Log In</a></li>
+                <li><Link onClick={props.onToggleMenu} to="#one">Projects</Link></li>
+                <li><Link onClick={props.onToggleMenu} to="#two">About</Link></li>
+                <li><Link onClick={props.onToggleMenu} to="#contact">Contact</Link></li>
             </ul>
         </div>
         <a className="close" onClick={props.onToggleMenu} href="javascript:;">Close</a>
--- src/pages/index.js
+++ src/pages/index.js
@@ -8,8 +8,8 @@ import pic01 from '../assets/images/blockchain.png'
 import pic02 from '../assets/images/postgods.png'
 import pic03 from '../assets/images/journey.png'
 import pic04 from '../assets/images/calendar.png'
-import pic05 from '../assets/images/pic05.jpg'
-import pic06 from '../assets/images/pic06.jpg'
+import pic05 from '../assets/images/instagram.png'
+
 
 class HomeIndex extends React.Component {
     render() {
@@ -17,7 +17,7 @@ class HomeIndex extends React.Component {
         return (
             <Layout>
                 <Helmet
-                    title="Gatsby Starter - Forty"
+                    title="Rosler Oriol"
                     meta={[
                         { name: 'description', content: 'Sample' },
                         { name: 'keywords', content: 'sample, something' },
@@ -34,43 +34,44 @@ class HomeIndex extends React.Component {
                                 <h3>Python Blockchain</h3>
                                 <p>Project created to improve my skills using python.</p>
                                 <h5>Tools Used:</h5>
-                                <ul>
+                                <ul className="tools-items">
                                     <li>Python</li>
                                     <li>Git</li>
                                     <li>Github</li> 
                                     <li>Visual Studio</li>
                                 </ul>
-                                <button>Github</button>
+                               <span className="projectlink"><a href="http://www.yahoo.com">View on Github</a></span>
                             </header>
                                 
 
-                            <Link to="/landing" className="link primary"></Link>
+                           
                         </article>
                         <article style={{backgroundImage: `url(${pic02})`}}>
                             <header className="major">
                                 <h3>Post Gods</h3>
                                 <p>A one page WordPress website developed for the Post Gods podcast.</p>
                                 <h5>Tools Used</h5>
-                                    <ul>
-                                        <li>HTML</li>
+                                    <ul className="tools-items">
+                                        <li>HTML5</li>
                                         <li>Wordpress</li>
                                         <li>Vagrant</li>
                                         <li>Github</li>
                                         <li>CSS3</li>
                                         <li>Virtual Box</li>
                                     </ul>
-                                    <button>Github</button>
-                                    <button>Live Website</button>
+                                    <span className="projectlink"><a href="http://www.yahoo.com">View on Github</a></span>
+                                   
+                                    <span className="projectlink"><a href="http://www.yahoo.com">View Live Website</a></span>
 
                             </header>
-                            <Link to="/landing" className="link primary"></Link>
+                          
                         </article>
                         <article style={{backgroundImage: `url(${pic03})`}}>
                             <header className="major">
                                 <h3>Journey Of Entrepreneurship</h3>
                                 <p>A one page WordPress website developed for the Journey Of Entrepreneurship podcast.</p>
                                 <h5>Tools Used</h5>
-                                    <ul>
+                                    <ul className="tools-items">
                                         <li>HTML</li>
                                         <li>Wordpress</li>
                                         <li>Vagrant</li>
@@ -78,27 +79,54 @@ class HomeIndex extends React.Component {
                                         <li>CSS3</li>
                                         <li>Virtual Box</li>
                                     </ul>
-                                    <button>Github</button>
-                                    <button>Live Website</button>
+                                    <span className="projectlink"><a href="http://www.yahoo.com">View on Github</a></span>
+                                   
+                                   <span className="projectlink"><a href="http://www.yahoo.com">View Live Website</a></span>
                             </header>
-                            <Link to="/landing" className="link primary"></Link>
+                            
                         </article>
                         <article style={{backgroundImage: `url(${pic04})`}}>
                             <header className="major">
                                 <h3>Javascript Calendar</h3>
                                 <p>A responsive Javascript Calendar created for the KOPMN website to advertise upcoming events.</p>
                                     <h5>Tools used:</h5>
-                                    <ul>
+                                    <ul className="tools-items">
                                         <li>Javascript</li>
                                         <li>CSS3</li>
                                         <li>Git</li>
                                         <li>Github</li>
                                         <li>HTML</li>
                                     </ul>
-                                    <button>Github</button>
+                                    <span className="projectlink"><a href="http://www.yahoo.com">View on Github</a></span>
                            
                             </header>
-                            <Link to="/landing" className="link primary"></Link>
+                           
+                        </article>
+                        <article style={{backgroundImage: `url(${pic05})`}}>
+                            <header className="major">
+                                <h3>Instagram Clone</h3>
+                                <p>A responsive Javascript Calendar created for the KOPMN website to advertise upcoming events.</p>
+                                    <h5>Tools used:</h5>
+                                    <ul className="tools-items">
+                                        <li>React.JS</li>
+                                    </ul>
+                                    <span className="projectlink"><a href="http://www.yahoo.com">View on Github</a></span>
+                           
+                            </header>
+                         
+                        </article>
+                        <article style={{backgroundImage: `url(${pic05})`}}>
+                            <header className="major">
+                                <h3>APP</h3>
+                                <p>A responsive Javascript Calendar created for the KOPMN website to advertise upcoming events.</p>
+                                    <h5>Tools used:</h5>
+                                    <ul className="tools-items">
+                                        <li>React.JS</li>
+                                    </ul>
+                                    <span className="projectlink"><a href="http://www.yahoo.com">View on Github</a></span>
+                           
+                            </header>
+                         
                         </article>
                     </section>
                     <section id="two">
@@ -110,7 +138,7 @@ class HomeIndex extends React.Component {
                             <p>I have 8 years of experience editing podcast, over 100 episodes, with my most recent projects being Post Gods and the Journey Of Entrepreneurship. I enjoy the creative process of helping podcasters share their stories, with listeners around the world.</p>
                             <p>I became a Toastmaster January 1st, 2017. During my time as a Toastmaster, I have had the opportunity to participate in several speech contests as a contestant and facilitator. I am currently serving as the Vice President of Public Relations for the City Of Delray Beach Toastmasters.</p>
                             <ul className="actions">
-                                <li><Link to="/landing" className="button next">Contact Me</Link></li>
+                                <li><Link to="#contact" className="button next">Contact Me</Link></li>
                             </ul>
                         </div>
                     </section>
